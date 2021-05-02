fcf3020c9372c7fc652583d74ebf8e060349f83b
Ks1204 <ks1204@scarletmail.rutgers.edu>
2021-05-01 20:30:43 -0400

    add

---
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.appsdeveloperblog.app.ws.ui.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+
+@RestController
+  //http://localhost:8080/users
+public class UserController {
+	
+	@GetMapping
+	public String getUser() {
+		return "get user was called";
+	}
+	
+	@PostMapping
+	public String createUser() {
+		return "create user was called";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "update user was called";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "delete user was called";
+	}
+
+	
+}
